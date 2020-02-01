e84562cdd16fecfa76c2f05b696d5021d33c6dcb
Ali Yusuf <w2bhnp@gmail.com>
2020-02-01 20:57:04 +0600

    define main layout in app.jsx and render it in the

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div role="main" className="container">
+				<h1>Hello World!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
+import App from "./app";
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
