c75126af55fb6ebe01013ea82b33d8f77aa09418
Ali Yusuf <w2bhnp@gmail.com>
2020-02-02 21:53:46 +0600

    move code to movies component from app component

---
+++ src/components/movies.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends React.Component {
+
+	state = {
+		movies: []
+	};
+
+	componentDidMount() {
+		this.setState({ movies: getMovies()})
+	}
+
+	render() {
+		return (
+			<div role="main" className="container">
+				{this.moviesCount()}
+				<hr/>
+				{this.movieTable()}
+			</div>
+		);
+	}
+
+	movieTable = () => {
+		if (this.state.movies.length > 0) {
+			return (
+				<div className="table-responsive">
+					<table className="table table-bordered">
+						<thead>
+						<tr>
+							<th>Title</th>
+							<th>Genre</th>
+							<th>Stock</th>
+							<th>Rate</th>
+							<th/>
+						</tr>
+						</thead>
+
+						<tbody>{this.state.movies.map(this.movieTableRow)}</tbody>
+					</table>
+				</div>
+			)
+		}
+	};
+
+	movieTableRow = movie => {
+		return (
+			<tr key={movie._id}>
+				<td>{movie.title}</td>
+				<td>{movie.genre.name}</td>
+				<td>{movie.numberInStock}</td>
+				<td>{movie.dailyRentalRate}</td>
+				<td>
+					<button
+						className="btn btn-danger btn-sm"
+						onClick={() => this.deleteMovieHandler(movie._id)}
+					>
+						Delete
+					</button>
+				</td>
+			</tr>
+		)
+	};
+
+	deleteMovieHandler = id => {
+		let movies = this.state.movies.filter(movie => movie._id !== id);
+		this.setState({ movies });
+		console.log('Deleted movie id ', id)
+	};
+
+	moviesCount = () => {
+		if (this.state.movies.length === 0) return <p>There are no movies in the database</p>
+		return <p>There are {this.state.movies.length} movies in the database</p>
+	}
+}
+
+export default Movies;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
-import App from "./app";
+import Movies from "./components/movies";
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Movies />, document.getElementById("root"));
