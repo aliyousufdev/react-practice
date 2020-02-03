08682134f9511296ee1d44c7764559a1f2929876
Ali Yusuf <w2bhnp@gmail.com>
2020-02-03 22:02:08 +0600

    refactor counter protions in 3 differents function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Counter extends Component {
 
@@ -7,24 +7,45 @@ class Counter extends Component {
 	};
 
 	handleIncrement = () => {
-		this.setState({ value: this.state.value + 1 });
+		this.setState({value: this.state.value + 1});
 	};
 
 	render() {
 		return (
 			<div>
-				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-				<button
-					onClick={this.handleIncrement}
-					className="btn btn-secondary btn-sm"
-				>
-					Increment
-				</button>
-
-				<button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+				{this.counterValue()}
+				{this.incrementButton()}
+				{this.decrementButton()}
 			</div>
 		);
 	}
+
+	counterValue = () => (
+		<span
+			style={{minWidth: 50}}
+			className={this.getBadgeClasses()}
+		>
+			{this.formatCount()}
+		</span>
+	);
+
+	incrementButton = () => (
+		<button
+			onClick={this.handleIncrement}
+			className="btn btn-secondary btn-sm"
+		>
+			Increment
+		</button>
+	);
+
+	decrementButton = () => (
+		<button
+			onClick={() => this.props.onDelete(this.props.id)}
+			className="btn btn-danger btn-sm m-2"
+		>
+			Delete
+		</button>
+	);
 
 	getBadgeClasses() {
 		let classes = "badge m-2 badge-";
@@ -33,7 +54,7 @@ class Counter extends Component {
 	}
 
 	formatCount() {
-		const { value } = this.state;
+		const {value} = this.state;
 		return value === 0 ? "Zero" : value;
 	}
 }
