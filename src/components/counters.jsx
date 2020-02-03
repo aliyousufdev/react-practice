7a05b2583c60a8089ed28a2b78e11e0da61bdd9d
Ali Yusuf <w2bhnp@gmail.com>
2020-02-03 20:56:09 +0600

    render counters array

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Counter from "./counter";
+import {map, prop} from "ramda";
 
 class Counters extends React.Component {
+
+	state = {
+		counters: [
+			{id: 1, value: 0},
+			{id: 2, value: 0},
+			{id: 3, value: 0},
+			{id: 4, value: 0},
+			{id: 5, value: 0},
+		]
+	};
+
 	render() {
+		const counters = prop('counters', this.state);
 		return (
 			<div>
-				<Counter />
-				<Counter />
-				<Counter />
-				<Counter />
+				{
+					map(c => <Counter key={c.id} />, counters)
+				}
 			</div>
 		);
 	}
