42da113b440421c84eae3f9387acd74c11eae100
Ali Yusuf <w2bhnp@gmail.com>
2020-02-07 20:22:43 +0600

    pagination basic

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { times } from "ramda";
 
-const Pagination = (props) => {
-	const perPage = props.perPage || 4;
-	if (props.itemCounts < perPage) return null;
+const Pagination = ({ itemCounts, perPage, onPageChange}) => {
+	if (itemCounts < perPage) return null;
+
+	const totalPage = Math.ceil(itemCounts / perPage);
+	const items = times(item, totalPage);
 
 	return (
 		<nav aria-label="Page navigation example">
 			<ul className="pagination justify-content-center">
-				<li className="page-item"><a className="page-link" href="/">1</a></li>
-				<li className="page-item"><a className="page-link" href="/">2</a></li>
-				<li className="page-item"><a className="page-link" href="/">3</a></li>
+				{items}
 			</ul>
 		</nav>
 	)
+};
+
+const item = idx => (
+	<li key={idx} className="page-item">
+		<a className="page-link" href="/">{idx+1}</a>
+	</li>
+);
+
+Pagination.defaultProps = {
+	perPage: 8
 };
 
 Pagination.propTypes = {
