ab1b65387af37a8b340083c5bf18fef309d10d7d
Ali Yusuf <w2bhnp@gmail.com>
2020-02-11 20:48:49 +0600

    create component GenereFilter

---
+++ src/components/common/genreFilter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const GenreFilter = props => {
+	return (
+		<ul className="list-group">
+			<li className="list-group-item">Cras justo odio</li>
+			<li className="list-group-item">Dapibus ac facilisis in</li>
+			<li className="list-group-item">Morbi leo risus</li>
+			<li className="list-group-item">Porta ac consectetur ac</li>
+			<li className="list-group-item">Vestibulum at eros</li>
+		</ul>
+	)
+};
+
+export default GenreFilter;
